b97a94a59bcc41b3e787bbfc3ae9a56d9779bfbe
giacomo9999 <gmarchesi888@gmail.com>
2020-07-16 14:43:41 -0400

    Write more tests.

--- Tests/index.test.js
+++ Tests/index.test.js
@@ -10,12 +10,23 @@ describe("Test App component", () => {
   let shallowWrapper, mountWrapper;
   beforeAll(() => {
     shallowWrapper = shallow(<App />);
-    // mountWrapper = mount(<App />);
+    mountWrapper = mount(<App />);
 
     console.log(shallowWrapper.debug());
-    // console.log(mountWrapper.debug());
+    console.log(mountWrapper.debug());
   });
+
   test("Should not return an error", () => {
     expect(shallowWrapper).toMatchSnapshot();
+  });
+
+  test("Should have an 'App' component at index 0", () => {
+    // console.log("position 1:", shallowWrapper.at(0).debug());
+    expect(shallowWrapper.at(0).type()).toBe("div");
+  });
+
+  test("Should have a child component of 'Header' at index 0", () => {
+    expect(mountWrapper.childAt(0).type()).toBe("div");
+    expect(shallowWrapper.childAt(0).type()).toBe(Header);
   });
 });
